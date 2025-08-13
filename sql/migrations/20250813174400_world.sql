311e6045ff399039b3888998db0f8e927899bfc2
ivanifer <ivgrekow@gmail.com>
2025-08-13 12:56:49 +0600

    fix table name in migration file

--- sql/migrations/20250813174400_world.sql
+++ sql/migrations/20250813174400_world.sql
@@ -9,7 +9,7 @@ INSERT INTO `migrations` VALUES ('20250813174400');
 -- Add your query below.
 
 
-UPDATE mangos.spell_template SET `script_name` = 'spell_rogue_sap'
+UPDATE spell_template SET `script_name` = 'spell_rogue_sap'
 WHERE entry IN (6770, 2070, 11297);
 
 
